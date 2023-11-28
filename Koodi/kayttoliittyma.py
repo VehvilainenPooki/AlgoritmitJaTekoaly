00eeb0c23d0f46afedc3e18d3c662980e4604629
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-11-28 14:56:26 +0200

    kuvaajaruutu lisätty. Prosessoi nappula ei toimi vielä.

--- Koodi/kayttoliittyma.py
+++ Koodi/kayttoliittyma.py
@@ -57,6 +57,54 @@ class taajuudenpoisto:
         #Työn alla
         print("Sijainti: ", self.tiedosto.get(), ", Taajuus: ", self.taajuus.get(), "Hz, Voimakkuus: ", self.voimakkuus.get(), "Db")
 
+class kuvaajaruutu:
+    def __init__(self, ikkuna=Tk):
+        self.ruutu = ttk.Frame(ikkuna)
+        self.ruutu.grid(column=0, row=0, sticky=(N, W, E, S))
+
+        ttk.Label(self.ruutu, text="Valitse tiedosto:").grid(column=1, row=1, sticky=E)
+        self.tiedosto = StringVar()
+        tiedosto_teksti = ttk.Label(self.ruutu, text="")
+        tiedosto_teksti.grid(column=3, row=1, sticky=E)
+        tiedosto_syote = ttk.Button(self.ruutu, text="Selaa:", command=lambda: self.valitse_tiedosto(sijainti=self.tiedosto, teksti=tiedosto_teksti))
+        tiedosto_syote.grid(column=2, row=1, sticky=(W, E))
+        
+
+        ttk.Label(self.ruutu, text="Suoritetaan fft").grid(column=1, row=2, sticky=E)
+        self.suoritetaanko_fft = IntVar(value=0)
+        suoritetaanko_fft_syote = ttk.Checkbutton(
+            self.ruutu, width=7, variable=self.suoritetaanko_fft)
+        suoritetaanko_fft_syote.grid(column=2, row=2, sticky=(W, E))
+
+        ttk.Label(self.ruutu, text="Suoritetaan ifft").grid(column=1, row=3, sticky=W)
+        self.suoritetaanko_ifft = IntVar(value=0)
+        suoritetaanko_ifft_syote = ttk.Checkbutton(
+            self.ruutu, width=7, variable=self.suoritetaanko_ifft)
+        suoritetaanko_ifft_syote.grid(column=2, row=3, sticky=(W, E))
+
+        ttk.Button(self.ruutu, text="Prosessoi", command=lambda: self.prosessoi()).grid(column=3, row=4, sticky=W)
+
+        for lapsi in self.ruutu.winfo_children(): 
+            lapsi.grid_configure(padx=5, pady=5)
+
+    def valitse_tiedosto(self, sijainti, teksti):
+        dir = cwd() + "/Syotteet/"
+        tiedosto = filedialog.askopenfilename(initialdir=dir, filetypes=[('Audio',['*.wav', '*.mp3'])])
+        sijainti.set(tiedosto)
+        i = tiedosto.rfind("/")
+        if i != -1:
+            tiedosto = tiedosto[i+1: ]
+
+        teksti.config(text=tiedosto)
+
+    def prosessoi(self):
+        #Työn alla
+        print("Sijainti: ", self.tiedosto.get(), ", Suoritetaanko fft: ", self.suoritetaanko_fft.get(), "Suoritetaanko ifft: ", self.suoritetaanko_ifft.get())
+
+    
+    def vaihda_ruutuun(self):
+        self.ruutu.tkraise()
+
 class aloitusruutu:
 
     def __init__(self, ikkuna=Tk):
@@ -110,6 +158,7 @@ class kayttoliittyma:
     def __init__(self, ikkuna):
         ikkuna.title("Kohinanpoistotyökalu")
         self.taajuuden_poisto = taajuudenpoisto(ikkuna)
+        self.kuvaaja_ruutu = kuvaajaruutu(ikkuna)
         self.aloitus_ruutu = aloitusruutu(ikkuna)
         self.valikko = valikko(self, ikkuna)
 
@@ -121,7 +170,7 @@ class kayttoliittyma:
         elif vaihdettavan_nimi == 'voimakkain taajuus':
             1
         elif vaihdettavan_nimi == 'kuvaaja':
-            1
+            self.kuvaaja_ruutu.vaihda_ruutuun()
         elif vaihdettavan_nimi == 'vertaile toteutuksia':
             1
         else:
