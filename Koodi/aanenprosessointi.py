93dc40fda2c1eb4dfaabfca91894ee8215fa736f
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-12-02 15:52:00 +0200

    Luotu poista_voimakkain_signaali, joka poistaa voimakkaimman signaalin fftdatasta.

--- Koodi/aanenprosessointi.py
+++ Koodi/aanenprosessointi.py
@@ -102,6 +102,53 @@ def FFT_tiedostolle(tiedostonOsoite, omaFFTtoteutus=True):
 
     return _suorita_FFT_datalle(_kasvata_data_sopivan_pituiseksi(data))
 
+def poista_voimakkain_signaali(fftData=np.array, poistoLeveys=int):
+    '''
+    Poistaa FFT prosessoidusta yksi-ulotteisesta datasta voimakkaimman signaalin.
+
+    Muuttujat:
+    fftData = fft:llä prosessoitu äänidata
+    poistoLeveys = Kuinka monta otosta poistetaan voimakkaimman signaalin kummaltakin puolelta
+
+    : Palauttaa prosessoidun datan
+    '''
+    data = fftdata.copy()
+    voimakkainKohta = _voimakkain_signaali(data)
+    return _poista_signaali(data, voimakkainKohta, poistoLeveys)
+
+def _voimakkain_signaali(fftData=np.array):
+    '''
+    Etsii fft datasta voimakkaaimman data pisteen ja palauttaa sen indexin
+    '''
+    voimakkain = 0
+    voimakkainKohta = -1
+    datanPituus = int(len(fftData)/2)
+    for kohta in range(datanPituus):
+        arvo = fftData[kohta]
+        if voimakkain < abs(arvo):
+            voimakkain = abs(arvo)
+            voimakkainKohta = kohta
+    return voimakkainKohta
+    
+def _poista_signaali(fftData=np.array, poistoKohta=int, poistoLeveys=int):
+    '''
+    Asettaa <poistoKohta> muuttujan kohdan ympäriltä <poistoLeveys> muuttujan kokoisen välin nollaksi
+
+    Muuttujat:
+    fftData = fft prosessoitu äänidata
+    poistoKohta = Kohta, jonka ympäriltä nollataan data
+    poistoLeveys = Leveys, joka nollataan poistoKohdan ympäriltä.
+
+    Palauttaa muunnetun fftData taulukon
+    '''
+    datanPituus = len(fftData)
+    for i in range(poistoLeveys*2+1):
+        kohta = poistoKohta+i-poistoLeveys
+        if datanPituus/2 > kohta and -1 < kohta:
+            fftData[kohta]=0+0j
+            fftData[datanPituus-kohta]=0+0J
+            
+    return fftData
 
 
 if __name__ == "__main__":
@@ -115,15 +162,41 @@ if __name__ == "__main__":
     #tiedosto = './Syotteet/Testi2Siniaaltoja.wav'
     tiedosto = './Syotteet/Testi3Siniaaltoja2.wav'
 
+
+
     otsikko, (kuvaaja1, kuvaaja2, kuvaaja3) = plt.subplots(3)
     otsikko.suptitle('FFT vertailu')
-    kuvaaja1.plot(FFT_tiedostolle(tiedosto, True),'r')
 
-    kuvaaja2.plot(FFT_tiedostolle(tiedosto, False),'r')
 
-    turha, data = wavfile.read(tiedosto)
+    #Generoidaan FFT data: True = käytetään omaa fft, False = käytetään scipy.fft (nopeampi)
+    fftdata = FFT_tiedostolle(tiedosto, False)
+
+    #Piirretään signaalinpoistoalue
+    voimakkainKohta = _voimakkain_signaali(fftdata)
+    poistonLeveys = 100
+    kuvaaja1.axvline(x=voimakkainKohta+poistonLeveys, color='b')
+    kuvaaja1.axvline(x=voimakkainKohta-poistonLeveys, color='b')
+    # Piirretään fftdata. Voit rajata valintaa lisäämällä plot kohtaan fftdata[:lopetuspiste]
+    kuvaaja1.plot(fftdata[:],'r')
+
+
+
+
+    #Piirretään fftdata, josta on poistettu voimakkain signaali.
+    # Voit rajata valintaa lisäämällä plot kohtaan voimakkainPoistettu[:lopetuspiste]
+    voimakkainPoistettu = poista_voimakkain_signaali(fftdata, poistonLeveys)
+    kuvaaja2.plot(voimakkainPoistettu[:],'r')
+
+    #Piirretään kuvaaja äänitiedostosta
+    otostiheys, data = wavfile.read(tiedosto)
     kuvaaja3.plot(data.T[0], 'r')
 
+    #Luo wav Tiedoston, josta on poistettu voimakkain signaali.
+    #Huomaa, että tällä hetkellä FFT on vain yksikanavainen,
+    #joten luodun tiedoston laatu ei ole ihan yhtä hyvä.
+    #Toiminnan huomaa parhaiten Testi1SiniAallolla.wav ja vertaa tulosta Testi1IlmanSini.wav
+    wavfile.write(filename="testi.wav",data=fft.ifft(x=voimakkainPoistettu).astype(np.int16), rate=otostiheys)
+    
     plt.show()
 
 
