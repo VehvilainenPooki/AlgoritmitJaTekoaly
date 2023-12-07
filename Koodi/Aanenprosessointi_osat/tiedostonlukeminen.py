731d362d79339b77c23594d8b75e365f661d4c02
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-12-07 15:24:47 +0200

    Tarkensin docString:ejä.

--- Koodi/Aanenprosessointi_osat/tiedostonlukeminen.py
+++ Koodi/Aanenprosessointi_osat/tiedostonlukeminen.py
@@ -8,7 +8,9 @@ import math
 
 def _kasvata_data_sopivan_pituiseksi(data):
     '''
-    Tarkistaa, että data on muotoa pituus=2^n. Jos ei ole, lisätään dataan 2^n-pituus nollaa.
+    Tarkistaa, että <data> on muotoa pituus=2^n. Jos ei ole, lisätään dataan 2^n-pituus nollaa.
+
+    Palauttaa nollilla kasvatetun taulukon muotoa 2^n.
     '''
     pituus = len(data)
     log2pituus = np.log2(pituus)
@@ -19,6 +21,15 @@ def _kasvata_data_sopivan_pituiseksi(data):
     return data
 
 def lue_wav_tiedosto(polku):
+    '''
+    Lukee <polku> osoitteessa olevan .wav tiedoston ja prosessoi sen fft algoritmilleni sopivaksi.
+
+    Palauttaa
+    <naytteenottoTaajuus> .wav tiedoston näytteenottotaajuus
+    <data> .wav tiedoston data tauluna,
+        johon on lisätty perään nollia niin, että taulun pituus on muotoa 2**n.
+        Nollien lisäys on fft algoritmia varten.
+    '''
     naytteenottoTaajuus, data = wavfile.read(polku)
 
     data = data.T[0]
